85ffc0b6397b36ad8ed5ee69ec8d186329b44548
ThiboDeBelie <thibo.debelie@student.uhasselt.be>
2024-06-09 08:38:34 +0200

    rename pages

---
+++ resources/views/loginZelf.blade.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Tailwind css  --> 
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body>
+    @include('./components/header')
+
+	<body class="bg-gray-100">
+		<div class="flex items-center justify-center h-screen">
+			<form class="w-full max-w-md bg-white shadow-md rounded-lg border-2 border-greenNav p-6" action="?/login" method="POST">
+				<h1 class="text-greenNav font-bold pb-3 text-2xl">Log in</h1>
+				<div class="mb-4">
+					<label for="email" class="block text-gray-700 font-semibold mb-2">E-mail</label>
+					<input type="email" id="email" name="email" placeholder="E-mail" class="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-greenNav">
+				</div>
+				<div class="mb-4">
+					<label for="password" class="block text-gray-700 font-semibold mb-2">Wachtwoord</label>
+					<input type="password" id="password" name="password" placeholder="Wachtwoord" class="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-greenNav">
+				</div>
+				<button type="submit" class="w-full bg-greenNav hover:bg-greenNav text-white hover:text-redKLJ font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:border-greenNav">Login</button>
+			</form>
+		</div>
+
+	</body>
+
+
+    @include('./components/footer')
+</body>
+</html>
---
+++ resources/views/logoutZelf.blade.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Tailwind css  --> 
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body class="bg-gray-100">
+    @include('./components/header')
+
+    <div class="flex items-center justify-center h-screen">
+        <form action="?/logout" method="POST" class="w-full max-w-md bg-white shadow-md rounded-lg p-6">
+            <h1 class="text-greenNav font-bold pb-3 text-2xl">Logout</h1>
+            <img src="{{ asset('img/logout.png') }}" alt="tets" class="mx-auto mb-4">
+            <p class="text-gray-600">Bent u zeker dat u wilt uitloggen ?</p>
+
+            <button type="submit" class="w-full bg-redKLJ hover:bg-greenNav text-white hover:text-redKLJ font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-500">Logout</button>
+            <button class="w-full bg-greenNav hover:bg-greenNav text-white hover:text-redKLJ font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-500 mt-3 mb-5" ><a href="../">Neen, surf verder</a></button>
+        </form>
+    </div>
+    
+    @include('./components/footer')
+</body>
+</html>
+
+
+{{-- <body class="bg-gray-100">
+    <div class="flex items-center justify-center h-screen">
+        <form action="?/logout" method="POST" class="w-full max-w-md bg-white shadow-md rounded-lg p-6">
+            <h1 class="text-greenNav font-bold pb-3 text-2xl">Logout</h1>
+            <img src="src\images\logout.png" alt="tets" class="mx-auto mb-4">
+            <p class="text-gray-600">Bent u zeker dat u wilt uitloggen ?</p>
+
+            <button type="submit" class="w-full bg-redKLJ hover:bg-greenNav text-white hover:text-redKLJ font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-500">Logout</button>
+            <button class="w-full bg-greenNav hover:bg-greenNav text-white hover:text-redKLJ font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:border-blue-500 mt-3 mb-5" on:click={() => {goto('/home')}}>Neen, surf verder</button>
+        </form>
+    </div>
+</body> --}}
+
+{{-- {{ asset('img/logout.png') }} --}}
